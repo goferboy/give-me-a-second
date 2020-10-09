f406963825eea84d1d422e313fe58535c6768825
Henry Baum <henryandrewbaum@gmail.com>
2020-10-09 10:41:43 -0400

    Added 20 more songs

--- js/song-data.js
+++ js/song-data.js
@@ -118,5 +118,125 @@ const songList = [
         artist: "Ween", 
         available: true, 
         path: "audio/020.mp3"
+    },
+    {
+        title: "The Message", 
+        artist: "Grandmaster Flash & The Furious 5", 
+        available: true, 
+        path: "audio/021.mp3"
+    },
+    {
+        title: "Planet Rock", 
+        artist: "Afrika Bambaataa", 
+        available: true, 
+        path: "audio/022.mp3"
+    },
+    {
+        title: "Oingo Boingo", 
+        artist: "Only A Lad", 
+        available: true, 
+        path: "audio/023.mp3"
+    },
+    {
+        title: "The Ohio Players", 
+        artist: "Skin Tight", 
+        available: true, 
+        path: "audio/024.mp3"
+    },
+    {
+        title: "Blister In The Sun", 
+        artist: "Violent Femmes", 
+        available: true, 
+        path: "audio/025.mp3"
+    },
+    {
+        title: "Adoption", 
+        artist: "Les Savy Fav", 
+        available: true, 
+        path: "audio/026.mp3"
+    },
+    {
+        title: "Antenne", 
+        artist: "Kraftwerk", 
+        available: true, 
+        path: "audio/027.mp3"
+    },
+    {
+        title: "A Little Respect", 
+        artist: "Erasure", 
+        available: true, 
+        path: "audio/028.mp3"
+    },
+    {
+        title: "Bed Of Roses", 
+        artist: "Statler Brothers", 
+        available: true, 
+        path: "audio/029.mp3"
+    },
+    {
+        title: "Generator", 
+        artist: "Bad Religion", 
+        available: true, 
+        path: "audio/030.mp3"
+    },
+    {
+        title: "While My Guitar Gently Weeps", 
+        artist: "George Harrison", 
+        available: true, 
+        path: "audio/031.mp3"
+    },
+    {
+        title: "Smack My Bitch Up", 
+        artist: "Prodigy", 
+        available: true, 
+        path: "audio/032.mp3"
+    },
+    {
+        title: "Hot For Teacher", 
+        artist: "Van Halen", 
+        available: true, 
+        path: "audio/033.mp3"
+    },
+    {
+        title: "Get Ur Freak On", 
+        artist: "Missy Elliot", 
+        available: true, 
+        path: "audio/034.mp3"
+    },
+    {
+        title: "10 Minutes", 
+        artist: "The Get Up Kids", 
+        available: true, 
+        path: "audio/035.mp3"
+    },
+    {
+        title: "Spending All My Time", 
+        artist: "Perfume", 
+        available: true, 
+        path: "audio/036.mp3"
+    },
+    {
+        title: "Can't Get You Out Of My Head", 
+        artist: "Kylie Minogue", 
+        available: true, 
+        path: "audio/037.mp3"
+    },
+    {
+        title: "My Generation", 
+        artist: "The Who", 
+        available: true, 
+        path: "audio/038.mp3"
+    },
+    {
+        title: "Cheerleader", 
+        artist: "St. Vincent", 
+        available: true, 
+        path: "audio/039.mp3"
+    },
+    {
+        title: "Enter Sandman", 
+        artist: "Metallica", 
+        available: true, 
+        path: "audio/040.mp3"
     }
 ];
